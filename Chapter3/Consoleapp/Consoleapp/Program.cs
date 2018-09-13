efc18b7bfaadbfaadafcceeb585655cff5253e7b
NevLo <pilleychristian@gmail.com>
2018-09-12 20:12:33 -0700

    inclass exercises for Chapter 3

---
+++ Chapter3/Consoleapp/Consoleapp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoleapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum;
+            int secondNum;
+            Console.Write("Please enter the first num: ");
+            firstNum = int.Parse(Console.ReadLine());
+            Console.Write("Please enter the second num: ");
+            secondNum = Convert.ToInt32(Console.ReadLine());
+
+            if(firstNum < 0)
+            {
+                Console.WriteLine("Hey!");
+                Console.WriteLine("That number is too small");
+            }
+        }
+    }
+}
---
+++ Chapter3/TestingApplication/TestingApplication/supercoolthreads.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+namespace TestingApplication
+{
+    //Please add 'public' to your class declarations
+    
+    public class Supercoolthreads
+    {
+        private static char CMD = 'a';
+
+        /*We added public to this method declaration as well */
+        public static void Main(string[] args)
+        {
+            //Variable dec
+            string name = "beth";
+            string lname;
+            int num1 = 4;
+            int num2 = 10;
+            int result;
+
+            
+            // string interpolation!
+            Console.Write("Please enter a last name: ");
+            lname = Console.ReadLine();
+            Console.WriteLine($"Hello {name} {lname}");
+
+            //prompt for values
+            Console.Write("Please enter num1: ");
+            num1 = int.Parse(Console.ReadLine());
+            Console.Write("Please enter num2: ");
+            string temp = Console.ReadLine();
+            num2 = int.Parse(temp);
+
+            //Add two variables and display results
+            result = num1 / num2;
+            Console.WriteLine($"{num1} / {num2} is {result}");
+            
+            
+            //Prevents app from exiting without permission from user
+            Console.ReadKey();
+          
+        }
+
+      
+       
+    }
+}
