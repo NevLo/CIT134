41bdcb9bb0fe51a7cba6ea15d879a046d33979ca
NevLo <pilleychristian@gmail.com>
2018-09-19 19:58:24 -0700

    Added Chapter 4 stuff, and opened Chapter 3 stuff

---
+++ Chapter4/CarTest/CarTest/Car.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarTest
+{
+    public class Car
+    {
+        private int numberOfWheels;
+        private string color;
+        private decimal price;
+        private int numberOfWindows;
+        
+
+        //Default Constructor
+        public Car() { }
+
+        //Overloaded constructor
+
+        public Car(int numberOfWheels, string color, decimal price)
+        {
+            this.numberOfWheels = numberOfWheels;
+            this.color = color;
+            this.price = price;
+        }
+        // Return the price of the car
+        public decimal GetPrice()
+        {
+            return price;
+
+
+        }
+
+        //autoimplementing property
+        public string Model { get; set; }
+
+
+        //number of windows property
+        public int NumberOfWindows
+        {
+            get
+            {
+                return numberOfWindows;
+            }
+            set
+            {
+                numberOfWindows = value;
+            }
+        }
+
+        //Writes all the car info to console
+        public void DisplayInfo()
+        {
+            Console.WriteLine("\nColor: {0}",color);
+            Console.WriteLine("Price: {0:C}",price);
+            Console.WriteLine("Wheels: {0}", numberOfWheels);
+
+        }
+
+        //Updates all the car info
+        public void UpdateInfo(int n, string c, decimal p)
+        {
+            numberOfWheels = n;
+            color = c;
+        
+            if(p < 5000.00M)
+            {
+                price = 5000.00M;
+            }
+            else
+            {
+                price = p;
+            }
+        }
+    }
+}
---
+++ Chapter4/CarTest/CarTest/CarTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarTest
+{
+    public class CarTest
+    {
+        public static void Main(string[] args)
+        {
+            //Instantiation
+            Car carOne = new Car();
+            Car carTwo = new Car();
+            Car carThree = new Car(2, "Black", 120000.00M);
+
+            carOne.UpdateInfo(4, "white", 45000.00M);
+            carTwo.UpdateInfo(5, "red", 2300.00M);
+            
+
+
+            carOne.DisplayInfo();
+            carTwo.DisplayInfo();
+            carThree.DisplayInfo();
+
+
+            decimal c1Price = carOne.GetPrice();
+            Console.WriteLine($"Price: {c1Price:C}");
+
+            carOne.NumberOfWindows = 7;
+            Console.WriteLine(carOne.NumberOfWindows);
+
+            carThree.Model = "Gremlin";
+
+            Console.WriteLine(carThree.Model);
+        }
+    }
+}
