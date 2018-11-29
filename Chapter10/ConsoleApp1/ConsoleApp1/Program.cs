4b7db69ad4e62fbab0d72c7330380b219b4bf31d
NevLo <pilleychristian@gmail.com>
2018-11-28 19:03:53 -0800

    Added inclass Assignment

---
+++ Chapter10/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            TestingThings t1 = new TestingThings(12, "Frodo is chill");
+            Console.WriteLine("Test Num: {0}", t1.TestNum);
+
+            t1.NumberOfTests = 900000000;
+            Console.WriteLine("Number of Tests: {0}", t1.getNumberOfTests());
+
+            Console.WriteLine("4 * PI {0}", 4 * TestingThings.PI);
+
+            Console.WriteLine("Test Name: {0}", t1.TestName);
+
+            try
+            {
+                t1.InputByte(9, 0);
+                t1.InputByte(2, 1);
+                t1.InputByte(5, 2);
+                t1.InputByte(8, 3);
+                t1.InputByte(9, 4);
+                t1.InputByte(2, 5);
+                t1.InputByte(5, 6);
+                t1.InputByte(6, 7);
+                t1.InputByte(6, 8);
+                t1.InputByte(7, 9);
+                t1.InputByte(1, 5);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine("Large number: {0}", t1);
+
+
+            Console.WriteLine("Goodbye!");
+        }
+    }
+}
---
+++ Chapter10/ConsoleApp1/ConsoleApp1/TestingThings.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class TestingThings
+    {
+        //Private instance variable
+        private int testNum;
+
+        //Setter is public, getter is private
+        public long NumberOfTests { private get; set; }
+
+        // constant for pi
+        public const double PI = 3.1415927;
+
+        // read only variable initialzed in the constructor
+        public readonly string TestName;
+
+        // private byte array
+
+        private byte[] bytes;
+
+        //Overloaded Constructor
+        public TestingThings(int testNum, string testName)
+        {
+            TestNum = testNum;
+            TestName = testName;
+            bytes = new byte[10];
+        }
+
+
+        //public property for testNum
+        public int TestNum
+        {
+            get { return testNum; }
+            private set { testNum = value; }
+        }
+        /// <summary>
+        /// Number of tests times the test number
+        /// </summary>
+        /// <returns></returns>
+        public long getNumberOfTests()
+        {
+            return NumberOfTests *testNum;
+        }
+        /// <summary>
+        /// Input values into byte array.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="index"></param>
+        public void InputByte(byte value, byte index)
+        {
+            if (index < 10 && value < 10)
+            {
+                bytes[index] = value;
+                return;
+            }
+            throw new ArgumentException("Index / Value must be less than 10");
+        }
+
+        /// <summary>
+        /// Returns a string representation of the bye array value
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            var result = "";
+            foreach(var v in bytes)
+            {
+                result += v;
+            }
+
+            return result;
+        }
+
+
+
+    }
+}
